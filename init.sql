54178ffc8d8ccc67ea97e4052736015c516700d8
Roberto Paes <robertocpaes@gmail.com>
2022-10-11 13:55:18 -0300

    adicionando url do postgree

--- init.sql
+++ init.sql
@@ -20,7 +20,7 @@ ADD FOREIGN KEY (id) REFERENCES usuarios_config (id)
 on delete cascade on update cascade
 DEFERRABLE INITIALLY DEFERRED;
  
-ALTER TABLE usuarios_config 
+ALTER TABLE usuarios_config
 ADD FOREIGN KEY (id) REFERENCES usuarios (id)
 on delete cascade on update cascade
 DEFERRABLE INITIALLY DEFERRED;
